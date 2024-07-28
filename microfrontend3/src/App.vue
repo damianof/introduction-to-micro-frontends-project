7329a8d7cd59459bed4dbf651b05eac48c303824
damianof <damianof@users.noreply.github.com>
2024-07-28 17:38:56 -0400

    pretty

--- microfrontend3/src/App.vue
+++ microfrontend3/src/App.vue
@@ -5,7 +5,9 @@ import Counter from './components/Counter.vue'
 // retrieve our postbox instance from window using the usePostbox hook
 const postbox = window.usePostbox()
 // subscribe to a topic
-postbox.sub('all-modules-loaded', (data) => console.log(`microfrontend3 (vue): received event "all-modules-loaded" with data:`, data))
+postbox.sub('all-modules-loaded', (data) =>
+  console.log(`microfrontend3 (vue): received event "all-modules-loaded" with data:`, data),
+)
 </script>
 
 <template>
