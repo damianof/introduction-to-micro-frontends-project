d795efb10ee40875d85cf523b2130a72becafebd
damianof <damianof@users.noreply.github.com>
2024-09-05 12:10:57 -0400

    temp changes

--- microfrontend3/src/App.vue
+++ microfrontend3/src/App.vue
@@ -2,6 +2,9 @@
 import { onMounted } from 'vue'
 import Counter from './components/Counter.vue'
 import CoffeeByCountry from './components/coffee-data/CoffeeByCountry.vue'
+import { IFileInfo, IFileValidatorOptions, FileUploadComponent 
+
+} from '../../../builtwithjavascript/vue-file-upload/src'
 
 // retrieve our postbox instance from window using the usePostbox hook
 const postbox = window.usePostbox()
@@ -12,11 +15,29 @@ onMounted(() => {
     console.log(`microfrontend3 (vue): received event "all-modules-loaded" with data:`, data),
   )
 })
+
+const fileValidatorOptions: IFileValidatorOptions = {
+  allowedTypes: ['csv', 'json', 'txt'],
+  maxSize: 3, // in MB
+  maxNameLength: 60, // max name length in chars
+  nameTruncateMaxLength: 35, // will truncate the display of the name
+  propertiesToValidate: ['name', 'type', 'size']
+}
+
+const onUploadClicked = async (fileInfo: IFileInfo) => {
+  // do what you need to do with fileInfo.file
+  // i.e. create form data and post it to an API endpoint
+  const file = new FormData()
+  file.append('file', fileInfo.file as Blob)
+}
 </script>
 
 <template>
   <div class="flex flex-col space-y-2">
-    <Counter />
-    <CoffeeByCountry v-show="true"/>
+    <FileUploadComponent id="123" uploadLabel="Testing" 
+      :fileValidatorOptions="fileValidatorOptions"
+      @uploadClicked="onUploadClicked"/>
+    <!-- <Counter />
+    <CoffeeByCountry v-show="true"/> -->
   </div>
 </template>
